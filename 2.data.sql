62ad428cad8da3c2533492f67e117b4d57d9751c
Hyosung0708 <tlsgytjd0218@gmail.com>
2023-09-04 10:37:00 +0900

    sql test

---
+++ 2.data.sql
@@ -0,0 +1,28 @@
+-- INSERT INTO Post (title, content)
+-- VALUES ('first post', 'hihi');
+
+-- INSERT INTO Post (title, content)
+-- VALUES ('hihi', 'test');
+
+-- -- INSERT INTO Post (title, content)
+-- -- VALUES ('hello');
+
+-- INSERT INTO Post (title, content)
+-- VALUES
+-- ('1', '1'),
+-- ('2', '2'),
+-- ('1', '2');
+
+
+-- INSERT INTO Comment (post_id, content)
+-- VALUES
+--     (1, 'hello'),
+--     (1, 'hello'),
+--     (2, 'hello'),
+--     (1, 'hello'),
+--     (3, 'hello'),
+--     (3, 'hello'),
+--     (5, 'hello');
+
+
+SELECT * FROM Comment;
